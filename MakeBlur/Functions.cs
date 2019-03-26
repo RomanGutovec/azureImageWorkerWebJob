9ab4d783ce09a5c844588971cb16e2755830aa4f
Roman Gutovec <roman.gutovec@gmail.com>
2019-03-26 12:33:06 +0300

    Add project MakeBlure, which make image with blur effect.

---
+++ MakeBlur/Functions.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using AForge.Imaging.Filters;
+using Microsoft.Azure.WebJobs;
+
+namespace MakeBlur
+{
+    public class Functions
+    {
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        public static void ProcessQueueMessage([QueueTrigger("imagemessagequeue")] string message,
+             [Blob("blobcontainer/{queueTrigger}", FileAccess.Read)] Stream myBlob,
+             [Blob("blobcontainer/{queueTrigger}", FileAccess.Write)] Stream outBlob,
+             TextWriter log)
+        {                  
+            Blur filter = new Blur();
+
+            Bitmap bitmap = new Bitmap(myBlob);
+
+            ImageFormat imageFormat = bitmap.RawFormat;
+            filter.ApplyInPlace(bitmap);
+            
+            bitmap.Save(outBlob, imageFormat);            
+        }
+    }
+}
